8a568d09bb75ac383fffb1f2d655a3183dab60c2
huangminhang <mhesat@outlook.com>
2021-11-17 10:01:24 +0800

    demo: py orm sqlalchemy demo

---
+++ py/sqlalchemy_demo/main.py
@@ -0,0 +1,39 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('sqlite:///test.db', echo=True)
+Base = declarative_base(engine)
+
+
+class User(Base):
+
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(20))
+    password = Column(String(32))
+
+
+Base.metadata.create_all()
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
+class SQL3(object):
+
+    def __init__(self):
+        pass
+
+    def insert(self):
+        user = User(name='Trunk', password='qwe')
+        session.add(user)
+        session.commit()
+        return None
+
+
+if __name__ == '__main__':
+    print("Hello SQLAlchemy!")
+    sql = SQL3()
+    sql.insert()
