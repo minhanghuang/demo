3bb2f725b7ff61a0747852f965a2817c0455aede
huangminhang <mhesat@outlook.com>
2021-11-19 15:09:24 +0800

    demo: python ros pub

---
+++ py/ros_pubsub_demo/pub.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding:UTF-8 -*-
+import rospy
+from std_msgs.msg import String
+
+
+if __name__ == '__main__':
+
+    print("pub")
+    rospy.init_node('talker', anonymous=True)
+    pub = rospy.Publisher('/chatter', String, queue_size=1)
+    rate = rospy.Rate(10)
+    while not rospy.is_shutdown():
+        hello_str = "hello world %s" % rospy.get_time()
+        pub.publish(hello_str)
+        rate.sleep()
