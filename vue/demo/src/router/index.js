ef9262f0abd16b78037ffbc1467ec6a616fe6c5c
huangminhang <mhesat@outlook.com>
2021-11-24 10:20:25 +0800

    demo: vue demo

--- vue/demo/src/router/index.js
+++ vue/demo/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/components/Home'
+import Home from '@/components/HelloWorld'
 
 Vue.use(Router);
 
@@ -8,8 +8,8 @@ export default new Router({
     routes: [
         {
             path: '/',
-            name: 'Home',
-            component: Home
+            name: 'HelloWorld',
+            component: HelloWorld
         }
     ]
 })
