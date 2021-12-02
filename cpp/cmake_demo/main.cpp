eb2a33bf7e25a0e3615cd5fc431c2fd0e6945f7e
huangminhang <mhesat@outlook.com>
2021-12-02 11:36:20 +0800

    update: cpp eigen

---
+++ cpp/cmake_demo/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, Cmake!" << std::endl;
+    return 0;
+}
--- cpp/eigen_demo/main.cpp
+++ cpp/eigen_demo/main.cpp
@@ -4,17 +4,26 @@
 
 int main() {
     std::cout << "Hello, Eigen!" << std::endl;
-    Eigen::Matrix2d m_matrix; // 2*2矩阵 double类型
-    m_matrix<< 2,3,2.2,1;
-    std::cout << "m_matrix = \n"<< m_matrix <<std::endl;
+    Eigen::Matrix2d matrix; // 2*2矩阵 double类型
+    matrix<< 2,3,2,1;
+
+    std::cout << "matrix = \n"<< matrix <<std::endl;
+
+    Eigen::MatrixXd matrix_x(2,2);
+    matrix_x<<1,1,2,2;
+//    matrix_x<<2,2;
+    std::cout << "matrix_x = \n"<< matrix_x <<std::endl;
+//    matrix_x<<2,2;
+//    std::cout << "matrix_x = \n"<< matrix_x <<std::endl;
+    Eigen::Vector2d v(1,2);
 
     Eigen::MatrixXf m_matrix_zero = Eigen::MatrixXf::Zero(5,5); // 零矩阵
 
     Eigen::MatrixXf m_matrix_identity = Eigen::MatrixXf::Identity(5,5); // 对角矩阵
 
     // 矩阵的大小
-    std::cout<<m_matrix.cols()<<std::endl;
-    std::cout<<m_matrix.rows()<<std::endl;
+    std::cout<<matrix.cols()<<std::endl;
+    std::cout<<matrix.rows()<<std::endl;
 
     // // 更多操作: https://guyuehome.com/34163
     // // 官方库: https://gitlab.com/libeigen/eigen.git
