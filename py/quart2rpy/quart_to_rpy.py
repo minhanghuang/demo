967462be9a697c65763559b6605acdb4c3473b02
huangminhang <mhesat@outlook.com>
2022-02-16 13:53:54 +0800

    add: 四元素转欧拉角 python

---
+++ py/quart2rpy/quart_to_rpy.py
@@ -0,0 +1,33 @@
+import sys
+from math import cos, sin, pi, atan2, asin
+
+if __name__ == '__main__':
+    print("四元素转欧拉角")
+    # quaternion = {
+    #     "x": 0.0056370173099,
+    #     "y": -0.00269722484417,
+    #     "z": 0.477472473219,
+    #     "w": 0.878624485393,
+    # }
+    quaternion = {
+        "x": 0.00139625178541,
+        "y": -0.0025784220826,
+        "z": -0.423166269627,
+        "w": 0.906047300349,
+    }
+    euler = {
+        "roll": 0,
+        "pitch": 0,
+        "yaw": 0,
+    }
+    x = quaternion["x"]
+    y = quaternion["y"]
+    z = quaternion["z"]
+    w = quaternion["w"]
+
+    euler["roll"] = atan2(2 * (w * x + y * z), 1 - 2 * (x * x + y * y))
+    euler["pitch"] = asin(2 * (w * y - z * x))
+    euler["yaw"] = atan2(2 * (w * z + x * y), 1 - 2 * (z * z + y * y))
+
+    print("四元素: {}".format(quaternion))
+    print("欧拉角: {}".format(euler))
