1a09a8fb7e4617c947f312512c44ffbdbfb4857a
huangminhang <huangminhang@trunk.tech>
2022-04-07 19:15:50 -0700

    add ros2 ws demo

---
+++ cpp/ros2_ws_demo/README.md
@@ -0,0 +1,7 @@
+```shell
+source /opt/ros/dashing/setup.zsh
+colcon build
+source install/setup.zsh
+ros2 run pub1_demo pub1
+ros2 run pub2_demo pub2
+```
---
+++ cpp/ros2_ws_demo/src/pub1_demo/main.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/string.hpp>
+
+
+int main(int argc, char **argv) {
+    std::cout << "Hello, ROS2 Pub!" << std::endl;
+    rclcpp::init(argc, argv);
+    auto node = rclcpp::Node::make_shared("pub1_node");
+    auto chatter_pub = node->create_publisher<std_msgs::msg::String>("/pub1", 1);
+    rclcpp::WallRate r(5);
+    
+    std_msgs::msg::String msg;
+    int i = 0;
+    while (rclcpp::ok()) {
+        msg.data = "data: " + std::to_string(i++);
+        std::cout << "pub1 " << msg.data.c_str() << std::endl;
+        chatter_pub->publish(msg);
+        rclcpp::spin_some(node);
+        r.sleep();
+    }
+    rclcpp::shutdown();
+    return 0;
+}
