d10be11c170effff0c4e266e35d2093bd6fdcdee
minhanghuang <mhesat@outlook.com>
2022-08-14 15:34:52 +0800

    feat(ros2): add octomap

--- cpp/ros2_ws/src/octomap_demo/launch/octomap.launch.py
+++ cpp/ros2_ws/src/octomap_demo/launch/octomap.launch.py
@@ -1,28 +1,20 @@
 import os
+from ament_index_python.packages import get_package_share_directory
 import launch
 import launch.actions
 import launch.substitutions
 import launch_ros.actions
 
 def generate_launch_description():
+    config = os.path.join(get_package_share_directory('octomap_demo'), 'param', 'octomap.yaml') 
     return launch.LaunchDescription(
             [
                 launch_ros.actions.Node(
                     package='octomap_demo',
                     executable='octomap_demo',
-                    output='screen'
-                ),
-                launch_ros.actions.Node(
-                    package='octomap_server',
-                    executable='octomap_server_node',
                     output='screen',
-                    parameters=[{
-                        'resolution': 0.1,
-                        'frame_id': "map",
-                    }],
-                    remappings=[('/cloud_in', '/pub')],
+                    parameters = [config],
                 ),
-
             ],
         )
 
--- cpp/ros2_ws/src/octomap_demo/main.cc
+++ cpp/ros2_ws/src/octomap_demo/main.cc
@@ -1,41 +1,75 @@
 #include <cstddef>
 #include <cstdlib>
 #include <iostream>
-#include <pcl/point_types.h>
-#include <pcl_conversions/pcl_conversions.h>
+#include <memory>
+#include <sstream>
+#include <octomap_msgs/conversions.h>
+#include <octomap_msgs/msg/octomap.hpp>
 #include <rclcpp/rclcpp.hpp>
-#include <sensor_msgs/msg/detail/point_cloud2__struct.hpp>
-#include <sensor_msgs/msg/point_cloud2.hpp>
-#include <std_msgs/msg/string.hpp>
+#include <vector>
 
+typedef struct Point PointType;
+typedef std::vector<PointType> Points;
+struct Point {
+  size_t x;
+  size_t y;
+  size_t z;
+};
+
+void ParamPoints(const std::shared_ptr<rclcpp::Node> node, Points &pts) {
+  node->declare_parameter("points");
+  auto param_pts = node->get_parameter("points");
+  Point point;
+  size_t i = 0;
+  for (const auto &it : param_pts.as_integer_array()) {
+    if (0 == i) {
+      point.x = it;
+      i++;
+    } else if (1 == i) {
+      point.y = it;
+      i++;
+    } else if (2 == i) {
+      point.z = it;
+      pts.emplace_back(point);
+      i = 0;
+    }
+  }
+}
+
+void PointsToOctoMap(const Points &pts, octomap::ColorOcTree *octomap) {
+  octomap->clear();
+  unsigned int R = 216;
+  unsigned int G = 120;
+  unsigned int B = 255;
+
+  for (const auto& point : pts) {
+    octomap->updateNode(octomap::point3d(point.x, point.y, point.z), true);
+    octomap->setNodeColor(point.x, point.y, point.z, 0, 0, 0);
+  }
+}
+
+void OctoMapToMsg(octomap::ColorOcTree *octomap, octomap_msgs::msg::Octomap& msg) {
+  msg.header.frame_id = "map";
+  msg.binary = false;
+  octomap_msgs::fullMapToMsg(*octomap, msg);
+}
 
 int main(int argc, char **argv) {
-  std::cout << "Hello, ROS2 OctoMap!" << std::endl;
-
   rclcpp::init(argc, argv);
-  auto node = rclcpp::Node::make_shared("pcl_node");
-  auto pub = node->create_publisher<sensor_msgs::msg::PointCloud2>("/pub", 1);
-  size_t num_points = 100;
-  pcl::PointCloud<pcl::PointXYZRGB> cloud;
-  cloud.points.resize(num_points);
-  sensor_msgs::msg::PointCloud2 output_msg;
-
-  for (int i = 0; i < num_points; i++) {
-    cloud.points[i].x = 10 * rand() / (RAND_MAX + 1.0f);
-    cloud.points[i].y = 10 * rand() / (RAND_MAX + 1.0f);
-    cloud.points[i].z = 10 * rand() / (RAND_MAX + 1.0f);
-    cloud.points[i].r = (rand() % (255 + 1)); //(rand() % (b-a+1))+ a
-    cloud.points[i].g = (rand() % (255 + 1));
-    cloud.points[i].b = (rand() % (255 + 1));
-  }
-
-  pcl::toROSMsg(cloud, output_msg); 
-  output_msg.header.frame_id = "map";
-
+  auto node = rclcpp::Node::make_shared("octomap_demo_node");
+  auto pub =
+      node->create_publisher<octomap_msgs::msg::Octomap>("/pub_octomap", 1);
   rclcpp::WallRate r(5);
+
+  Points pts;
+  ParamPoints(node, pts);
+  octomap::ColorOcTree* octomap = new octomap::ColorOcTree(0.1);
+  octomap_msgs::msg::Octomap msg;
+  PointsToOctoMap(pts, octomap);
+  OctoMapToMsg(octomap, msg);
+
   while (rclcpp::ok()) {
-    RCLCPP_INFO(node->get_logger(), "pub");
-    pub->publish(output_msg);
+    pub->publish(msg);
     rclcpp::spin_some(node);
     r.sleep();
   }
