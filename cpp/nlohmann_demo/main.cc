1f1eb15d6e258489c94bd835dccb191a15575ddd
minhanghuang <mhesat@outlook.com>
2022-09-21 15:25:34 +0800

    feat: 更新json demo

--- cpp/nlohmann_demo/main.cc
+++ cpp/nlohmann_demo/main.cc
@@ -5,8 +5,11 @@
 int main() {
     std::cout << "Hello, nlohmann json!" << std::endl;
     // 1. 字符串转json
-    std::string s = "{ \"happy\": true, \"pi\": 3.141 }";
+    std::string s = "{ \"happy\": true, \"pi\": 3.141,  \"name\": \"\",  \"age\": 99 }";
     nlohmann::json js = nlohmann::json::parse(s);
     std::cout << js << std::endl;
+    std::cout << "name:" << js["name"].get<std::string>() << std::endl;
+    std::cout << "oooo:" << js["oooo"].is_null() << std::endl;
+    std::cout << "age:" << js["age"].is_null() << std::endl;
     return 0;
 }
