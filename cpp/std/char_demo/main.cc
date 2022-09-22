00a57a1a0d427d92b065d29bdb6d95c02a0983f6
minhanghuang <mhesat@outlook.com>
2022-09-22 10:20:02 +0800

    feat: update char demo

--- cpp/std/char_demo/main.cc
+++ cpp/std/char_demo/main.cc
@@ -1,11 +1,25 @@
+#include <iostream>
 #include <stdio.h>
+
 int main(int argc, char *argv[]) {
   // const char *p = "123456";
-  char *p;
+  // // char *p;
+  // if (p == NULL) {
+  //   printf("空");
+  // } else {
+  //   printf("不为空");
+  //   return 0;
+  // }
+
+
+  // std::string s;
+  std::string s = "1234";
+  const char *p = s.c_str();
   if (p == NULL) {
     printf("空");
   } else {
     printf("不为空");
     return 0;
   }
+
 }
