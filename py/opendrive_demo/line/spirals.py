676456fc8a81d41c7b4c8ab54a6c8b16c33956dd
minhanghuang <job@minhang.me>
2022-11-26 19:37:14 +0800

    perf: update opendrive spirals

---
+++ py/opendrive_demo/line/spirals.py
@@ -0,0 +1,51 @@
+import numpy as np
+from math import cos, sin, pi, radians, sqrt
+from scipy.special import fresnel
+import matplotlib.pyplot as plt
+
+
+def spiral_interp_centre(distance, x, y, hdg, length, curvEnd):
+    '''Interpolate for a spiral centred on the origin'''
+
+    # s doesn't seem to be needed...
+    theta = hdg                    # Angle of the start of the curve
+    Ltot = length                  # Length of curve
+    Rend = 1 / curvEnd             # Radius of curvature at end of spiral
+
+    # Rescale, compute and unscale
+    a = 1 / sqrt(2 * Ltot * Rend)  # Scale factor
+    distance_scaled = distance * a  # Distance along normalised spiral
+    deltay_scaled, deltax_scaled = fresnel(distance_scaled)
+    deltax = deltax_scaled / a
+    deltay = deltay_scaled / a
+
+    # deltax and deltay give coordinates for theta=0
+    deltax_rot = deltax * cos(theta) - deltay * sin(theta)
+    deltay_rot = deltax * sin(theta) + deltay * cos(theta)
+
+    # Spiral is relative to the starting coordinates
+    xcoord = x + deltax_rot
+    ycoord = y + deltay_rot
+
+    return xcoord, ycoord
+
+
+def main():
+    x_list = []
+    y_list = []
+    for n in range(100):
+        # x, y = spiral_interp_centre(n, 50, 100, radians(56), 40, 1/20.)
+        x, y = spiral_interp_centre(n/10, -2.4122510649719874e+01,
+                                    6.9999999999999991e+00,
+                                    -4.9666937229631003e-12,
+                                    1.0521739130434783e+01,
+                                    8.6956521739130432e-02)
+        x_list.append(x)
+        y_list.append(y)
+
+    plt.plot(x_list, y_list)
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
