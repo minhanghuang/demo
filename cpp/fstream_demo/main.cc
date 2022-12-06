c4c4fbf249aef615dbe3d621a1a278dbfbbc703c
minhanghuang <mhesat@outlook.com>
2022-12-06 15:28:37 +0800

    feat: add fstream cpp demo

---
+++ cpp/fstream_demo/main.cc
@@ -0,0 +1,20 @@
+#include <fstream>  // fstream
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+  std::cout << "Hello fstream." << std::endl;
+  /// - ofstream: 写
+  /// - ifstream: 读
+  /// - fstream: 读写
+  std::fstream fs;
+  /// std::ios::in 读
+  /// std::ios::out 写
+  /// std::ios::ate
+  /// std::ios::app 追加
+  /// std::ios::trunc 如果文件存在,先删除,再创建
+  /// std::ios::binary 二进制
+  fs.open("data.json", std::ios::out);
+  fs << "{'header':'bbb'}" << std::endl;
+  fs.close();  // 关闭句柄
+  return 0;
+}
