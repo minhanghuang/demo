b0fc2a972cc4d08bf45445b7cf52f3dc3babc623
minhanghuang <job@minhang.me>
2023-02-08 09:46:24 +0800

    perf(ros): update ros pub sub

--- cpp/ros_pubsub_demo/pub.cc
+++ cpp/ros_pubsub_demo/pub.cc
@@ -1,32 +1,17 @@
-/*
-* Copyright (C) Trunk Technology, Inc. - All Rights Reserved
-*
-* Unauthorized copying of this file, via any medium is strictly prohibited
-* Proprietary and confidential
-*
-* Written by Huang Minhang <huangminhang@trunk.tech>, 2021/11/23 15:02
-*/
-#include <iostream>
 #include <ros/ros.h>
 #include <std_msgs/String.h>
 
-
-int main(int argc, char **argv) {
-    std::cout << "Hello, ROS Pub!" << std::endl;
-    ros::init(argc, argv, "talker");
-    ros::NodeHandle n;
-    ros::Publisher pub = n.advertise<std_msgs::String>("chatter", 1);
-    ros::Rate loop_rate(10);
-    int count = 0;
-    while (ros::ok()) {
-        std_msgs::String msg;
-        std::stringstream ss;
-        ss << "hello world " << count;
-        msg.data = ss.str();
-        pub.publish(msg);
-        loop_rate.sleep();
-        ++count;
-    }
-    return 0;
+int main(int argc, char** argv) {
+  std::cout << "Hello, Ros Pub!" << std::endl;
+  ros::init(argc, argv, "pub");
+  ros::NodeHandle n;
+  ros::Publisher pub = n.advertise<std_msgs::String>("/chat", 1);
+  ros::Rate r(5);
+  std_msgs::String msg;
+  msg.data = "hello";
+  while (ros::ok()) {
+    pub.publish(msg);
+    r.sleep();
+  }
+  return 0;
 }
-
--- cpp/ros_pubsub_demo/sub.cc
+++ cpp/ros_pubsub_demo/sub.cc
@@ -1,29 +1,28 @@
-/*
-* Copyright (C) Trunk Technology, Inc. - All Rights Reserved
-*
-* Unauthorized copying of this file, via any medium is strictly prohibited
-* Proprietary and confidential
-*
-* Written by Huang Minhang <huangminhang@trunk.tech>, 2021/11/23 15:01
-*/
-#include <iostream>
+#include <ros/init.h>
 #include <ros/ros.h>
+#include <ros/spinner.h>
 #include <std_msgs/String.h>
 
-
-void chatterCallback(const std_msgs::String::ConstPtr& msg) {
-    std::cout << msg->data.c_str() << std::endl;
+void Chatcb(const std_msgs::String& msg) {
+  std::cout << msg.data.c_str() << std::endl;
 }
 
-int main(int argc, char **argv) {
-    std::cout << "Hello, ROS Sub!" << std::endl;
+int main(int argc, char** argv) {
+  std::cout << "Hello, Ros Sub!" << std::endl;
+  ros::init(argc, argv, "sub");
+  ros::NodeHandle n;
+  ros::Subscriber sub = n.subscribe("/chat", 10, Chatcb);
 
-    ros::init(argc, argv, "listener");
-    ros::NodeHandle n;
-    ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
-    ros::spin();
+  /// 单线程
+  // ros::spin();
+  // while (ros::ok()) {
+  //   ros::spinOnce();
+  // }
 
-    return 0;
+  /// 多线程
+  // ros::MultiThreadedSpinner spiner; // 默认机器线程数
+  ros::MultiThreadedSpinner spiner(3);  // 3个线程
+  spiner.spin();
+
+  return 0;
 }
-
-
