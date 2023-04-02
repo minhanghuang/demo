eca207d7cb79857be44085fc01eaa12811afc44c
minhanghuang <job@minhang.me>
2023-04-02 16:31:37 +0800

    feat: add vue press demo

---
+++ js/vuepress-demo/docs/.vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  title: '学习笔记',
+  description: '黄民航的学习笔记',
+  themeConfig: {
+    logo: '/assets/img/logo.png',
+    lastUpdated: 'Last Updated', // string | boolean
+    sidebar: 'auto',
+    nav: [
+      { text: '首页', link: '/' },
+      { text: 'Cpp', link: '/guide/cpp' },
+      { text: 'Git', link: '/guide/git' },
+      { text: 'Github', link: 'https://github.com/minhanghuang' },
+      { text: 'VuePress', link: 'https://v1.vuepress.vuejs.org/zh/' },
+    ]
+  }
+}
---
+++ js/vuepress-demo/docs/README.md
@@ -0,0 +1,8 @@
+---
+home: true
+heroImage: /assets/img/logo.png
+heroText: 学习笔记
+tagline: 最淡的墨水胜过最强的记忆 
+actionText: 开始 →
+actionLink: https://u1s1.vip
+---
