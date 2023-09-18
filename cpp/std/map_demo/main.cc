0064da9d79c413ff5c8129a4a050a3cdaa75e68c
minhanghuang <job@minhang.me>
2023-09-18 22:12:42 +0800

    chore: update cpp map

--- cpp/std/map_demo/main.cc
+++ cpp/std/map_demo/main.cc
@@ -1,21 +1,24 @@
+#include <cassert>
 #include <iostream>
 #include <map>
 #include <string>
 #include <unordered_map>
+#include <utility>
 
+int main(int argc, char* argv[]) {
+  std::cout << "Hello, map." << std::endl;
 
-
-int main(int argc, char *argv[]) {
-  std::cout << "std::map demo" << std::endl;
-  std::unordered_map<std::string, int> int_route;
-  for (int j = 0; j < 10; j++) {
-    int_route[std::to_string(j)] = j; 
+  {
+    // 使用insert或者emplace都不会替换原来已经存在的值
+    std::unordered_map<std::string, int> m;
+    m.insert(std::make_pair("1", 1));
+    assert(1 == m.size());
+    assert(m["1"] == 1);
+    m.insert(std::make_pair("1", 2));
+    assert(m["1"] == 1);
+    m.emplace(std::make_pair("1", 1));
+    assert(m["1"] == 1);
   }
-
-  std::cout << "获取值:" << int_route["2"] << std::endl;
-  std::cout << "获取值:" << int_route.at("2") << std::endl;
-  std::cout << "获取值(不存在):" << int_route["22"] << std::endl;
-  std::cout << "获取值(不存在):" << int_route.at("22") << std::endl;
 
   return 0;
 }
