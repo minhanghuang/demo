fcae69f402542682eeb460725185bb70ff97e5f8
minhanghuang <job@minhang.me>
2023-11-07 14:28:25 +0800

    feat(cpp): 更新grpc使用

--- cpp/grpc_demo/simple/client.cc
+++ cpp/grpc_demo/simple/client.cc
@@ -1,54 +1,50 @@
-/*
-* Copyright (C) Trunk Technology, Inc. - All Rights Reserved
-*
-* Unauthorized copying of this file, via any medium is strictly prohibited
-* Proprietary and confidential
-*
-* Written by Huang Minhang <huangminhang@trunk.tech>, 2021/11/11 17:14
-*/
-#include "../proto/demo.grpc.pb.h"
-#include <grpcpp/grpcpp.h>
-#include <iostream>
-#include <chrono>
 #include <unistd.h>
+
+#include <chrono>
+#include <iostream>
 #include <memory>
 
+#include <grpcpp/grpcpp.h>
+
+#include "proto/demo.grpc.pb.h"
+
 class DemoClient {
-public:
-    explicit DemoClient(const std::shared_ptr<grpc::Channel> &channel)
-        : m_service(demo::HelloService::NewStub(channel)) {}
+ public:
+  explicit DemoClient(const std::shared_ptr<grpc::Channel>& channel)
+      : m_service(demo::HelloService::NewStub(channel)) {}
 
-    int say_hello(const std::string& s) {
+  int say_hello(const std::string& s) {
+    demo::Request request;
+    demo::Reply reply;
+    grpc::ClientContext context;
+    grpc::ClientContext context2;
+    auto deadline =
+        std::chrono::system_clock::now() + std::chrono::milliseconds(100);
+    context.set_deadline(deadline);
+    context2.set_deadline(deadline);
+    std::cout << "hello" << std::endl;
+    grpc::Status status = m_service->SayHello(&context, request, &reply);
+    std::cout << "hello reply:" << status.error_code() << std::endl;
+    status = m_service->SayHello(&context2, request, &reply);
+    std::cout << "hello reply:" << status.error_code() << std::endl;
 
-        demo::Request request;
-        demo::Reply reply;
-        grpc::ClientContext context;
-        grpc::ClientContext context2;
-        auto deadline = std::chrono::system_clock::now() + std::chrono::milliseconds(100);
-        context.set_deadline(deadline);
-        context2.set_deadline(deadline);
-        std::cout << "hello" << std::endl;
-        grpc::Status status = m_service->SayHello(&context, request, &reply);
-        std::cout << "hello reply:" << status.error_code() << std::endl;
-        status = m_service->SayHello(&context2, request, &reply);
-        std::cout << "hello reply:" << status.error_code() << std::endl;
-
-        if (status.ok()) {
-            return 1;
-        } else {
-            std::cout << status.error_code() << ": " << status.error_message() << std::endl;
-            return 0;
-        }
+    if (status.ok()) {
+      return 1;
+    } else {
+      std::cout << status.error_code() << ": " << status.error_message()
+                << std::endl;
+      return 0;
     }
+  }
 
-private:
-    std::unique_ptr<demo::HelloService::Stub> m_service;
+ private:
+  std::unique_ptr<demo::HelloService::Stub> m_service;
 };
 
-int main(int argc, char **argv) {
-
-    DemoClient client(grpc::CreateChannel("127.0.0.1:9090", grpc::InsecureChannelCredentials()));
-    client.say_hello("world");
-    sleep(1);
-    return 0;
+int main(int argc, char** argv) {
+  DemoClient client(grpc::CreateChannel("127.0.0.1:9090",
+                                        grpc::InsecureChannelCredentials()));
+  client.say_hello("world");
+  sleep(1);
+  return 0;
 }
--- cpp/grpc_demo/simple/server.cc
+++ cpp/grpc_demo/simple/server.cc
@@ -1,33 +1,30 @@
-/*
-* Copyright (C) Trunk Technology, Inc. - All Rights Reserved
-*
-* Unauthorized copying of this file, via any medium is strictly prohibited
-* Proprietary and confidential
-*
-* Written by Huang Minhang <huangminhang@trunk.tech>, 2021/11/11 17:14
-*/
-#include <iostream>
 #include <unistd.h>
+
+#include <iostream>
+
 #include <grpcpp/grpcpp.h>
-#include "../proto/demo.grpc.pb.h"
+
+#include "proto/demo.grpc.pb.h"
 
 class HelloServiceImpl : public demo::HelloService::Service {
-public:
-    grpc::Status SayHello(::grpc::ServerContext* context, const ::demo::Request* request, ::demo::Reply* response) override {
-        std::cout << "say_hello" << std::endl;
-        sleep(1);
-        return grpc::Status::OK;
-    }
+ public:
+  grpc::Status SayHello(::grpc::ServerContext* context,
+                        const ::demo::Request* request,
+                        ::demo::Reply* response) override {
+    std::cout << "say_hello" << std::endl;
+    sleep(1);
+    return grpc::Status::OK;
+  }
 };
 
 int main() {
-    std::cout << "Hello, Simple Server!" << std::endl;
-    std::string address("0.0.0.0:9090");
-    HelloServiceImpl service;
-    grpc::ServerBuilder builder;
-    builder.AddListeningPort(address, grpc::InsecureServerCredentials());
-    builder.RegisterService(&service);
-    std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
-    server->Wait();
-    return 0;
+  std::cout << "Hello, Simple Server!" << std::endl;
+  std::string address("0.0.0.0:9090");
+  HelloServiceImpl service;
+  grpc::ServerBuilder builder;
+  builder.AddListeningPort(address, grpc::InsecureServerCredentials());
+  builder.RegisterService(&service);
+  std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+  server->Wait();
+  return 0;
 }
