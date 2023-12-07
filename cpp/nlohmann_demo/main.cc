f460dc740a562b4d965a21d0b31a55097798ad61
minhanghuang <job@minhang.me>
2023-12-07 13:00:14 +0800

    perf(cpp): 更新json例子

--- cpp/nlohmann_demo/main.cc
+++ cpp/nlohmann_demo/main.cc
@@ -1,15 +1,34 @@
 #include <iostream>
+
 #include <nlohmann/json.hpp>
 
-
 int main() {
-    std::cout << "Hello, nlohmann json!" << std::endl;
-    // 1. 字符串转json
-    std::string s = "{ \"happy\": true, \"pi\": 3.141,  \"name\": \"\",  \"age\": 99 }";
+  std::cout << "Hello, nlohmann json!" << std::endl;
+  {
+    std::cout << "@@@@@ 字符串转json @@@@@" << std::endl;
+    std::string s =
+        "{ \"happy\": true, \"pi\": 3.141,  \"name\": \"\",  \"age\": 99 }";
     nlohmann::json js = nlohmann::json::parse(s);
     std::cout << js << std::endl;
     std::cout << "name:" << js["name"].get<std::string>() << std::endl;
     std::cout << "oooo:" << js["oooo"].is_null() << std::endl;
     std::cout << "age:" << js["age"].is_null() << std::endl;
-    return 0;
+  }
+
+  {
+    std::cout << "@@@@@  移除不必要的空格 @@@@@" << std::endl;
+    nlohmann::json data;
+    for (int i = 0; i < 1000; i++) {
+      data[i][0] = i;
+      data[i][1] = i + 100;
+    }
+    // 用于指定缩进的空格数或制表符数。默认值为-1，表示不使用缩进，即生成压缩的JSON字符串。
+    // 如果将该参数设置为0或正整数，则输出的JSON字符串将使用该数量的空格或制表符进行缩进。例如，设置indent=4时，JSON字符串将缩进4个空格。
+    std::string dump_default = data.dump();
+    std::string dump_0 = data.dump(0);
+
+    std::cout << "default:" << dump_default.size() << std::endl;
+    std::cout << "0:" << dump_0.size() << std::endl;
+  }
+  return 0;
 }
