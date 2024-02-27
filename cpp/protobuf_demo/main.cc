e141ecb89bc336091ffdbae04947b159dc5550ec
minhanghuang <job@minhang.me>
2024-02-27 09:30:18 +0800

    perf(cpp): 更新Protobuf例子

--- cpp/protobuf_demo/main.cc
+++ cpp/protobuf_demo/main.cc
@@ -1,6 +1,8 @@
+#include <cassert>
 #include <iostream>
 
 #include <google/protobuf/timestamp.pb.h>
+#include <google/protobuf/util/time_util.h>
 
 #include "proto/hello.pb.h"
 
@@ -10,9 +12,31 @@ int main(int argc, char* argv[]) {
   {
     std::cout << "repeated 赋值" << std::endl;
     Person person = Person();
+    auto current_time = google::protobuf::util::TimeUtil::GetCurrentTime();
+    person.mutable_birth_date()->set_seconds(current_time.seconds());
+    person.mutable_birth_date()->set_nanos(0);
     person.add_hobbies("baseball");
     person.add_hobbies("football");
     std::cout << "person : \n" << person.DebugString() << std::endl;
+  }
+
+  {
+    std::cout << "repeated 取值" << std::endl;
+    Person person = Person();
+    auto current_time = google::protobuf::util::TimeUtil::GetCurrentTime();
+    person.mutable_birth_date()->set_seconds(current_time.seconds());
+    person.mutable_birth_date()->set_nanos(0);
+    person.add_hobbies("baseball");
+    person.add_hobbies("football");
+
+    assert("baseball" == person.hobbies()[0]);
+    assert("football" == person.hobbies()[1]);
+    assert("baseball" == person.hobbies(0));
+    assert("football" == person.hobbies(1));
+    std::cout << "hobby 0: " << person.hobbies()[0] << std::endl;
+    std::cout << "hobby 1: " << person.hobbies()[1] << std::endl;
+    std::cout << "hobby 0: " << person.hobbies(0) << std::endl;
+    std::cout << "hobby 1: " << person.hobbies(1) << std::endl;
   }
   return 0;
 }
