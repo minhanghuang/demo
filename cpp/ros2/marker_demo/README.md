6542fcf86b77e666a7addef9715b89ad873cef9a
minhanghuang <job@minhang.me>
2024-04-21 15:31:18 +0800

    perf(cpp): 更新ros2 marker例子

---
+++ cpp/ros2/marker_demo/README.md
@@ -0,0 +1,4 @@
+
+```shell
+colcon build && ros2 launch marker_demo marker_launch.py
+```
--- cpp/ros2/marker_demo/src/main.cc
+++ cpp/ros2/marker_demo/src/main.cc
@@ -11,46 +11,76 @@
 
 int main(int argc, char* argv[]) {
   rclcpp::init(argc, argv);
-  auto node = rclcpp::Node::make_shared("marker_node");
+  auto node = rclcpp::Node::make_shared("marker_demo_node");
 
-  // qos
   rclcpp::QoS qos(0);
 
+  // https://docs.ros2.org/foxy/api/visualization_msgs/msg/MarkerArray.html
   auto pub = node->create_publisher<visualization_msgs::msg::MarkerArray>(
-      "/ros2/lane_markers", 1);
+      "/ros2/demo/lane_markers", 1);
 
-  // 创建一个MarkerArray消息
   auto marker_array = visualization_msgs::msg::MarkerArray();
 
-  // 创建第一条车道线的Marker
-  visualization_msgs::msg::Marker marker;
-  marker.header.frame_id = "map";
-  marker.header.stamp = node->now();
-  marker.ns = "lane_markers";
-  marker.id = 1;
+  for (int i = 0; i < 2; i++) {
+    // 创建第一条车道线的Marker
+    visualization_msgs::msg::Marker marker;
+    marker.header.frame_id = "map";
+    marker.header.stamp = node->now();
+    marker.ns = "markers";
+    marker.id = i;
+    marker.action = visualization_msgs::msg::Marker::ADD;
 
-  // LINE_STRIP: 一条连接所有点的线段
-  // LINE_LIST: 每两个连续的点将会形成一条独立的线段
-  marker.type = visualization_msgs::msg::Marker::LINE_STRIP;
+    if (0 == i) {
+      // LINE_STRIP: 一条连接所有点的线段
+      // LINE_LIST: 每两个连续的点将会形成一条独立的线段
+      marker.type = visualization_msgs::msg::Marker::LINE_STRIP;
 
-  // ADD: 表示要添加一个新的 Marker
-  marker.action = visualization_msgs::msg::Marker::ADD;
+      marker.pose.orientation.w = 1.0;
 
-  marker.pose.orientation.w = 1.0;  // 四元素 设置为1.0,表示无旋转
+      marker.scale.x = 0.01;  // 线段宽度
+      marker.color.r = 1.0;   // RGB
+      marker.color.a = 1.0;   // 透明度
+                              //
+      geometry_msgs::msg::Point point;
+      point.set__x(1);
+      point.set__y(1);
+      marker.points.emplace_back(point);
+      point.set__x(2);
+      point.set__y(2);
+      marker.points.emplace_back(point);
+      point.set__x(3);
+      point.set__y(3);
+      marker.points.emplace_back(point);
+      point.set__x(4);
+      point.set__y(4);
+      marker.points.emplace_back(point);
 
-  marker.scale.x = 0.01;  // x表示线段的宽度
-  marker.color.r = 1.0;   // RGB
-  marker.color.a = 1.0;   // 代表透明度
+    } else if (1 == i) {
+      marker.type = visualization_msgs::msg::Marker::LINE_STRIP;
 
-  for (int i = 0; i < 3; i++) {
-    geometry_msgs::msg::Point point;
-    point.set__x(i);
-    point.set__y(i);
-    point.set__z(0);
-    marker.points.emplace_back(point);
+      marker.pose.orientation.w = 1.0;
+
+      marker.scale.x = 0.1;  // 线段宽度
+      marker.color.r = 1.0;  // RGB
+      marker.color.a = 1.0;  // 透明度
+
+      geometry_msgs::msg::Point point;
+      point.set__x(0);
+      point.set__y(0);
+      marker.points.emplace_back(point);
+      point.set__x(1);
+      point.set__y(2);
+      marker.points.emplace_back(point);
+      point.set__x(2);
+      point.set__y(3);
+      marker.points.emplace_back(point);
+      point.set__x(3);
+      point.set__y(4);
+      marker.points.emplace_back(point);
+    }
+
+    marker_array.markers.emplace_back(marker);
   }
-
-  marker_array.markers.emplace_back(marker);
 
   rclcpp::WallRate rate(5);
   while (rclcpp::ok()) {
