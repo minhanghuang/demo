debfd9e66459903663d14179fa30f9ca568a7a2e
minhanghuang <job@minhang.me>
2024-06-12 08:58:13 +0800

    update(cpp): 更新map例子

--- cpp/std/map_demo/main.cc
+++ cpp/std/map_demo/main.cc
@@ -1,4 +1,5 @@
 #include <cassert>
+#include <exception>
 #include <iostream>
 #include <map>
 #include <string>
@@ -8,17 +9,28 @@
 int main(int argc, char* argv[]) {
   std::cout << "Hello, map." << std::endl;
 
+  std::unordered_map<std::string, int> data;
+  for (int i = 0; i < 10; i++) {
+    data.insert({std::to_string(i), i});
+  }
+
   {
     std::cout << "@@@@@ 使用insert或者emplace都不会替换原来已经存在的值 @@@@@"
               << std::endl;
     std::unordered_map<std::string, int> m;
-    m.insert(std::make_pair("1", 1));
+
+    m.insert(std::make_pair("1", 1));  // {"1": 1}
     assert(1 == m.size());
     assert(m["1"] == 1);
-    m.insert(std::make_pair("1", 2));
+
+    m.insert(std::make_pair("1", 2));  // {"1": 1}
     assert(m["1"] == 1);
-    m.emplace(std::make_pair("1", 1));
+
+    m.emplace(std::make_pair("1", 1));  // {"1": 1}
     assert(m["1"] == 1);
+
+    m["1"] = 3;  // {"1": 3}
+    assert(m["1"] == 3);
   }
 
   {
@@ -34,6 +46,13 @@ int main(int argc, char* argv[]) {
               << std::endl;
     std::cout << "删除100: " << a.erase("100") << "剩余元素数量: " << a.size()
               << std::endl;
+  }
+
+  {
+    std::cout << "@@@@@ 获取第一个元素 @@@@@" << std::endl;
+    auto begin = data.begin();
+    std::cout << "begin key: " << begin->first << std::endl;
+    std::cout << "begin value: " << begin->second << std::endl;
   }
 
   return 0;
